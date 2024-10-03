1f127610b1aa817c57a099b906dfa97bf9350a9a
Ankit <iankitraj18@gmail.com>
2024-10-03 11:39:12 +0530

    Task

---
+++ ArrayListDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace PareshSir
+{
+    internal class ArrayListDemo
+    {
+
+        public static void Main()
+        {
+            //Creating an object of collection
+            ArrayList obj = new ArrayList();
+
+            //Adding elements into a cpllection
+            obj.Add("RKU");
+            obj.Add(125);
+            obj.Add(152.85);
+            obj.Add('X');
+            obj.Add(true);
+
+            //Fetching the no. of elements into collection
+            Console.WriteLine("Number of Elements: " + obj.Count);
+
+            //Removing an element by value from a collection
+            obj.Remove("RKU");
+
+
+            //Traversing a collection element by element
+            foreach (object str in obj) //object
+                Console.WriteLine(str + " ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArrayListDemo2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PareshSir
+{
+    internal class ArrayListDemo2
+    {
+        public static void Main()
+        {
+            SortedList S = new SortedList();
+            S.Add("22SOECE11172", "Ankit");
+            S.Add("22SOECE11170", "Raj");
+            S.Add("22SOECE11190", "Sheilendra");
+            S["22SOECE11170"] = "Anku";
+
+            for (int i = 0; i < S.Count; i++) //object
+                Console.WriteLine(S.GetByIndex(i));
+            Console.ReadKey();
+        }
+    }
+}
