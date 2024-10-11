80d6034eab9d901c0a7e38cc598b0cac836d1ca9
Ankit <iankitraj18@gmail.com>
2024-10-11 15:44:40 +0530

    Demo

---
+++ DictionaryDemo2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PareshSir
+{
+    internal class DictionaryDemo2
+    {
+        static void Main()
+        {
+            //Student s1 = new Student();
+            Dictionary<string, string> S = new Dictionary<string, string>();
+            S.Add("22SOECE11172", "Ankit");
+            S.Add("22SOECE11170", "Raj");
+            S.Add("22SOECE11190", "Sheilendra");
+            S["22SOECE11170"] = "Anku";
+
+            ICollection<string> c = S.Keys;
+            foreach (string t in c)
+                Console.WriteLine(S[t]);
+
+            Console.ReadKey();
+        }
+    }
+}
